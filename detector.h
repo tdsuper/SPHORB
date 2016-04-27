abc4d819ac93142b756deb7da58323b179b47c72
tdsuper <qiangzhao_tju@163.com>
2016-04-27 10:10:21 +0800

    Updated Bib

--- detector.h
+++ detector.h
@@ -1,7 +1,7 @@
 /*
 	AUTHOR:
 	Qiang Zhao, email: qiangzhao@tju.edu.cn
-	Copyright (C) 2014 Tianjin University
+	Copyright (C) 2015 Tianjin University
 	School of Computer Software
 	School of Computer Science and Technology
 
@@ -10,11 +10,14 @@
 	commercial licensing, please contact the authors at the contact address below.
 
 	REFERENCE:
-	@TECHREPORT{SPHORB2014,
-	author =       {Qiang Zhao and Wei Feng and Liang Wan and Jiawan Zhang},
-	title =        {SPHORB: A Fast and Robust Binary Feature on the Sphere},
-	institution =  {Tianjin University},
-	year =         {2014},
+	@article{zhao-SPHORB,
+	author   = {Qiang Zhao and Wei Feng and Liang Wan and Jiawan Zhang},
+	title    = {SPHORB: A Fast and Robust Binary Feature on the Sphere},
+	journal  = {International Journal of Computer Vision},
+	year     = {2015},
+	volume   = {113},
+	number   = {2},
+	pages    = {143-159},
 	}
 */
 
